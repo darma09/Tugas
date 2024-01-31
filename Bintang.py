d32ad9da951688f77754476f4fb82db1dc2fb06e
darma09 <55343445+darma09@users.noreply.github.com>
2024-01-31 14:46:32 +0700

    Update Bintang.py

--- Bintang.py
+++ Bintang.py
@@ -1,29 +1,30 @@
 import turtle
-import time
 import streamlit as st
 
-def bintang():    s = turtle.Screen()
+def bintang():
+    s = turtle.Screen()
     t = turtle.Turtle()
-    s.bgcolor('black')    t.pencolor('white')
+    s.bgcolor('black')
+    t.pencolor('white')
     t.speed(2)
     t.lt(100)
     t.fd(100)
     col = ('blue', 'red', 'white', 'green')
 
-    for i in range(4):
-        for n in range(4):
-            t.penup()
-            t.goto(0, -n * 40)
-            t.pendown()
-            t.pencolor(col[n % 4])
+    for n in range(4):
+        t.penup()
+        t.goto(0, -n * 40)
+        t.pendown()
+        t.pencolor(col[n % 4])
 
-            for _ in range(5):
-                t.forward(100)
-                t.right(144)
-            time.sleep(1) # Add a time delay of 1 second
+        for _ in range(5):
+            t.forward(100)
+            t.right(144)
+            
+    s.exitonclick()
 
 def main():
-    st.title("Turtle Animation")
+    st.title("Bintang Animation")
     st.write("Click the button to start the animation")
     if st.button("Start Animation"):
         bintang()
