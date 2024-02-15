8dc2d0150b007407d604934e3b4b9aecf6a7f3a6
darma09 <55343445+darma09@users.noreply.github.com>
2024-02-15 19:00:06 +0700

    Update Bintang.py

--- Bintang.py
+++ Bintang.py
@@ -17,7 +17,7 @@ st.title("KALKULATOR")
 # Display the name of the program creator
 st.header("💀Darma Alif Rakhaa💀")
 
-pilihan = st.selectbox("Pilih operasi:", ["Penjumlahan", "Pengurangan", "Perkalian", "Pembagian", "Persentase ke Angka", "Angka ke Persentase"])
+pilihan = st.selectbox("Pilih operasi:", ["Penjumlahan", "Pengurangan", "Perkalian", "Pembagian"])
 
 angka1 = int(st.number_input("Masukkan angka pertama:", value=1, step=1))
 angka2 = int(st.number_input("Masukkan angka kedua:", value=1, step=1))
@@ -32,23 +32,10 @@ elif pilihan == "Perkalian":
     hasil = kali(angka1, angka2)
     st.markdown(f"**Hasil perkalian:** {hasil}")
 elif pilihan == "Pembagian":
-    if angka2 != 0:
-        hasil = bagi(angka1, angka2)
-        st.markdown(f"**Hasil pembagian:** {hasil}")
-    else:
-        st.write("Tidak dapat membagi dengan angka 0")
-elif pilihan == "Persentase ke Angka":
-    persen = float(st.number_input("Masukkan persentase:", value=0, step=0.01))
-    hasil = persen / 100 * angka2
-    st.markdown(f"**Hasil konversi persentase ke angka:** {hasil}")
-elif pilihan == "Angka ke Persentase":
-    if angka2 != 0:
-        angka = float(st.number_input("Masukkan angka:", value=0, step=1))
-        persen = angka / angka2 * 100
-        st.markdown(f"**Hasil konversi angka ke persentase:** {persen}%")
-    else:
-        st.write("Tidak dapat mengkonversi dengan angka 0")
+    hasil = bagi(angka1, angka2)
+    st.markdown(f"**Hasil pembagian:** {hasil}")
 else:
     st.write("Pilihan tidak valid")
+
 
 st.caption('Copyleft © Darma Alif Rakhaa 2024')
