74a960005a11315bde77a965ba5e6af698577644
darma09 <55343445+darma09@users.noreply.github.com>
2024-02-15 18:58:14 +0700

    Update Bintang.py

--- Bintang.py
+++ Bintang.py
@@ -17,7 +17,7 @@ st.title("KALKULATOR")
 # Display the name of the program creator
 st.header("💀Darma Alif Rakhaa💀")
 
-pilihan = st.selectbox("Pilih operasi:", ["Penjumlahan", "Pengurangan", "Perkalian", "Pembagian"])
+pilihan = st.selectbox("Pilih operasi:", ["Penjumlahan", "Pengurangan", "Perkalian", "Pembagian", "Persentase ke Angka", "Angka ke Persentase"])
 
 angka1 = int(st.number_input("Masukkan angka pertama:", value=1, step=1))
 angka2 = int(st.number_input("Masukkan angka kedua:", value=1, step=1))
@@ -34,8 +34,15 @@ elif pilihan == "Perkalian":
 elif pilihan == "Pembagian":
     hasil = bagi(angka1, angka2)
     st.markdown(f"**Hasil pembagian:** {hasil}")
+elif pilihan == "Persentase ke Angka":
+    persen = float(st.number_input("Masukkan persentase:", value=0, step=0.01))
+    hasil = persen / 100 * angka2
+    st.markdown(f"**Hasil konversi persentase ke angka:** {hasil}")
+elif pilihan == "Angka ke Persentase":
+    angka = float(st.number_input("Masukkan angka:", value=0, step=1))
+    persen = angka / angka2 * 100
+    st.markdown(f"**Hasil konversi angka ke persentase:** {persen}%")
 else:
     st.write("Pilihan tidak valid")
-
 
 st.caption('Copyleft © Darma Alif Rakhaa 2024')
