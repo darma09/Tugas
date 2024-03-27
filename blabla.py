4e4c65822b07cc068dc2176a7d70ebe1881a6c34
darma09 <55343445+darma09@users.noreply.github.com>
2024-03-28 03:30:10 +0700

    Update blabla.py

--- blabla.py
+++ blabla.py
@@ -107,20 +107,18 @@ import streamlit as st
 st.write("""
 # Heart disease Prediction App
 
-This app predicts If a patient has a heart disease
-         jika tidak tahu apa atau maksudnya yang di input bisa baca pada informasi dibawah ini
-Information :
-         1. Masukan umur kalian
-         2. Jenis kelamin kalian (0: laki-laki, 1: perempuan)
-         3. Jenis nyeri dada (0: angina tipikal, 1: angina atipikal, 2: nyeri non-angina, 3: tanpa gejala)
-         atau kunjungi website : https://www.halodoc.com/artikel/ketahui-4-jenis-jenis-nyeri-dada-yang-perlu-diwaspadai
-         4. Tekanan darah istirahat
-         5. Kolesterol serum dalam mg/dl
-         6. Gula darah puasa > 120 mg/dl (1: true, 0: false)
-         7. Gula darah sehingga hasil elektrokardiografi istirahat > 90 mg/dl (1: normal, 0: abnormal)
-         8. EKG epidimiokardiografi maksimum yang dicapai adalah ≥ 50% QRS normal (1: ya, 0: tidak)
-        
-    
+This app predicts if a patient has a heart disease.
+
+Information:
+1. Masukan umur kalian (Enter your age)
+2. Jenis kelamin kalian (0: laki-laki, 1: perempuan) (Enter your gender: 0 for male, 1 for female)
+3. Jenis nyeri dada (0: angina tipikal, 1: angina atipikal, 2: nyeri non-angina, 3: tanpa gejala) (Enter chest pain type: 0 for typical angina, 1 for atypical angina, 2 for non-anginal pain, 3 for asymptomatic)
+    or visit website: https://www.halodoc.com/artikel/ketahui-4-jenis-jenis-nyeri-dada-yang-perlu-diwaspadai
+4. Tekanan darah istirahat (Enter resting blood pressure)
+5. Kolesterol serum dalam mg/dl (Enter serum cholestoral in mg/dl)
+6. Gula darah puasa > 120 mg/dl (1: true, 0: false) (Enter fasting blood sugar: 1 for true, 0 for false)
+7. Gula darah sehingga hasil elektrokardiografi istirahat > 90 mg/dl (1: normal, 0: abnormal) (Enter resting electrocardiographic results: 1 for normal, 0 for abnormal)
+8. EKG epidimiokardiografi maksimum yang dicapai adalah ≥ 50% QRS normal (1: ya, 0: tidak) (Enter maximum heart rate achieved: 1 for yes, 0 for no)
 """)
 
 st.sidebar.header('User Input Features')
